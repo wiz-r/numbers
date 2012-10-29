2223b5976385301655d540d4d4e3d763a884fbab
Hiroki Yagita <hiroki.yagita@gree.co.jp>
2012-10-29 23:10:31 +0900

    Added some view controllers

---
+++ Numbers/Numbers/GameViewController.m
@@ -0,0 +1,38 @@
+//
+//  GameViewController.m
+//  Numbers
+//
+//  Created by hiroki-yagita on 12/10/29.
+//  Copyright (c) 2012年 wiz-r. All rights reserved.
+//
+
+#import "GameViewController.h"
+
+@interface GameViewController ()
+
+@end
+
+@implementation GameViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
